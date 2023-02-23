019063e977b20247463628923b65aa61226ed846
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2023-02-23 10:41:08 +0530

    Create upgrade-cluster.sh

---
+++ upgrade-cluster.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+new_k8s_version=$1
+
+
+for i in $(kubectl get nodes | awk '{print $1}' | tail -n +2)
+do
+        if (( $i=="master" )); then
+
+		
+		      echo "Upgrading kubeadm on master"
+		      sudo apt-mark unhold kubeadm && \
+		      sudo apt-get update && sudo apt-get install -y kubeadm=$new_k8s_version-00 && \
+		      sudo apt-mark hold kubeadm
+
+		      sudo kubeadm upgrade apply v$new_k8s_version -y
+
+		      kubectl drain $i --ignore-daemonsets
+
+		      sudo apt-mark unhold kubelet kubectl && \
+		      sudo apt-get update && sudo apt-get install -y kubelet=$new_k8s_version-00 kubectl=$new_k8s_version-00 && \
+		      sudo apt-mark hold kubelet kubectl
+
+		      sudo systemctl daemon-reload
+		      sudo systemctl restart kubelet
+		      kubectl uncordon $i
+
+
+        else
+
+		      echo "Upgrading kubeadm on worker $i"
+		      kubectl cordon $i
+
+		      sudo kubectl drain $i --ignore-daemonsets
+
+		      ssh cloud_user@$i << EOF
+
+		      sudo apt-mark unhold kubeadm && \
+		      sudo apt-get update && sudo apt-get install -y kubeadm=$new_k8s_version-00 && \
+		      sudo apt-mark hold kubeadm
+
+		      sudo kubeadm upgrade node	
+
+		
+
+		      sudo apt-mark unhold kubelet kubectl && \
+		      sudo apt-get update && sudo apt-get install -y kubelet=$new_k8s_version-00 kubectl=$new_k8s_version-00 && \
+		      sudo apt-mark hold kubelet kubectl
+		      EOF
+
+		      sudo systemctl daemon-reload
+		      sudo systemctl restart kubelet
+		      kubectl uncordon $i
+
+		
+	fi
+
+done
