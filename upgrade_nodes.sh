1c264459cc06739ddb5a502beaa3badd52e230fb
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2023-02-23 17:43:04 +0530

    Update upgrade_nodes.sh

--- upgrade_nodes.sh
+++ upgrade_nodes.sh
@@ -5,11 +5,7 @@ new_k8s_version=$1
 sudo apt-mark unhold kubeadm && \
 sudo apt-get update && sudo apt-get install -y kubeadm=$new_k8s_version-00 && \
 sudo apt-mark hold kubeadm && \
-
 sudo kubeadm upgrade node && \
-
-
-
 sudo apt-mark unhold kubelet kubectl && \
 sudo apt-get update && sudo apt-get install -y kubelet=$new_k8s_version-00 kubectl=$new_k8s_version-00 && \
 sudo apt-mark hold kubelet kubectl
