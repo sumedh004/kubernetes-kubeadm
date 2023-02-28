0cbbd2bfe14e10ee65dffc732a124729cb2851a2
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2023-02-28 14:02:09 +0530

    Update upgrade-cluster.sh

--- upgrade-cluster.sh
+++ upgrade-cluster.sh
@@ -4,37 +4,39 @@ new_k8s_version=$1
 
 for i in $(kubectl get nodes | awk '{print $1}' | tail -n +2)
 do
-        if (( $i=="master" )); then
+        echo $i
+        if [ "$i" == "master" ]; then
 
 
                 echo "Upgrading kubeadm on master"
-                sudo apt-mark unhold kubeadm && \
-                sudo apt-get update && sudo apt-get install -y kubeadm=$new_k8s_version-00 && \
-                sudo apt-mark hold kubeadm && \
+                sudo apt-mark unhold kubeadm
+                sudo apt-get update && sudo apt-get install -y kubeadm=$new_k8s_version-00
+                sudo apt-mark hold kubeadm
 
-                sudo kubeadm upgrade apply v$new_k8s_version -y && \
+                sudo kubeadm upgrade apply v$new_k8s_version -y
 
-                kubectl drain $i --ignore-daemonsets && \
+                kubectl drain $i --ignore-daemonsets
 
-                sudo apt-mark unhold kubelet kubectl && \
-                sudo apt-get update && sudo apt-get install -y kubelet=$new_k8s_version-00 kubectl=$new_k8s_version-00 && \
-                sudo apt-mark hold kubelet kubectl && \
-                sudo systemctl daemon-reload && \
-                sudo systemctl restart kubelet && \
+                sudo apt-mark unhold kubelet kubectl
+                sudo apt-get update && sudo apt-get install -y kubelet=$new_k8s_version-00 kubectl=$new_k8s_version-00
+                sudo apt-mark hold kubelet kubectl
+                sudo systemctl daemon-reload
+                sudo systemctl restart kubelet
                 kubectl uncordon $i
 
 
         else
 
                 echo "Upgrading kubeadm on worker $i"
-                kubectl cordon $i && \
-                kubectl drain $i --ignore-daemonsets && \
+                kubectl cordon $i
+                kubectl drain $i --ignore-daemonsets
                 #echo "azureuser@$i"
                 #echo "ssh azureuser@$i "bash -s" < upgrade_nodes.sh "$1""
-                ssh -o StrictHostKeyChecking=no azureuser@${i} 'bash -s' < upgrade_nodes.sh $1
+                #echo "ssh azureuser@${i} 'bash -s' < upgrade_nodes.sh $1"
+                ssh azureuser@${i} 'bash -s' < upgrade_nodes.sh $1
                 #ssh azureuser@${i} 'bash -s' < upgrade_nodes.sh $1 && \
-                sudo systemctl daemon-reload && \
-                sudo systemctl restart kubelet && \
+                sudo systemctl daemon-reload
+                sudo systemctl restart kubelet
                 kubectl uncordon $i
 
 
